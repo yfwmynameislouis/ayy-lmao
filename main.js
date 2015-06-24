544c9b67dd65fae8ba9b369f8e05eef515599758
Louis David <louisd@dhcp-192-168-4-11.semantico.net>
2015-06-24 15:29:38 +0100

    ayy lmao: the game added. ayy lmao

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 alert('ayy lmao');
+
+var options = ['yes', 'no', 'ayy lmao', 'this isn\'t new york wtf is this'];
+
+// Returns a random number between min (inclusive) and max (exclusive)
+function getRandomArbitrary(min, max) {
+ return Math.round(Math.random() * (max - min) + min);
+}
+
+document.getElementsByClassName('activate')[0].onclick = function () {
+   var win = getRandomArbitrary(0,3);
+   var userValue = document.getElementById('toss').value;
+   document.getElementById('lmao-says').innerHTML= options[win];
+   if (options[win] === userValue) {
+     document.getElementById('result').innerHTML='you won! AYY LMAO, CONTINUE AYY LMAOING';
+   } else {
+   document.getElementById('result').innerHTML='you aren\'t worthy of an ayy lmao!';
+   }
+
+};
